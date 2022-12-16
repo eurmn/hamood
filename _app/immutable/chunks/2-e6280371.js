2d77fdb8e72a56f2d4f15397df3077c9af6dc98e
Lucas <euromoon@protonmail.com>
2022-12-16 17:21:27 -0300

    Updates

--- _app/immutable/chunks/2-e6280371.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-b4df577a.js";export{t as component};
---
+++ _app/immutable/chunks/2-ed9a4640.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-c4500f25.js";export{t as component};
