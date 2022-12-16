7d3bc97f373fb0b448f289e138ffe2b7abe292e5
Lucas <euromoon@protonmail.com>
2022-12-16 17:21:11 -0300

    Fixed img bug (?).

--- src/components/Character.svelte
+++ src/components/Character.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
+    import { base } from "$app/paths";
     export let width: number;
     export let height: number;
     export let scale: number;
     export let character: string;
 
-    const values: [number, number, number, string] = [width, height, scale, character];
+    const values: [number, number, number, string] = [
+        width,
+        height,
+        scale,
+        character,
+    ];
 </script>
 
 <!-- svelte-ignore a11y-missing-attribute -->
-<img class="animate-spin absolute select-none"
+<img
+    class="animate-spin absolute select-none"
     style={`left: ${values[0]}%; top: ${values[1]}%;`}
     width={values[2] * 332}
     height={values[2] * 477}
-    src={`${values[3]}.png`}
+    src={`base/${values[3]}.png`}
 />
